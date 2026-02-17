b77dd15a07134637c77537f05110d2af0937639a
Blake Wilson <bwilson0908@yahoo.com>
2026-02-17 02:53:11 -0800

    Built VarsAndMath.java program

---
+++ VarsAndMath.java
@@ -0,0 +1,15 @@
+public class VarsAndMath {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 3;
+        double price = 2.5;
+
+        int sum = a + b;
+        double total = sum * price;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("Sum = " + sum);
+        System.out.println("Total cost = $" + total);
+    }
+}
